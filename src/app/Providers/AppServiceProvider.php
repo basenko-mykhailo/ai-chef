e4c3061a0f4c1e5daf52b0bd91171f7137884b33
Mykhailo Basenko <michaelb@warmy.io>
2026-06-09 09:09:50 +0300

    update code by https

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS URL generation when the app is served over TLS (driven by
+        // APP_URL), so assets/links aren't emitted as http:// behind the proxy.
+        if (str_starts_with((string) config('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 }
